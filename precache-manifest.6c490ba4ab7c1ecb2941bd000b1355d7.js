1e5c4baa66fbb861b3e75f6d93fb5427236ea2c4
Dale The Wizard <dale.m.allen@hotmail.com>
2019-12-10 16:34:22 -0500

    Updates

---
+++ precache-manifest.6c490ba4ab7c1ecb2941bd000b1355d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa47b8ea697eafc84400f70629193489",
+    "url": "/dalemallen.github.io/index.html"
+  },
+  {
+    "revision": "b147ff2d7b8452c32b23",
+    "url": "/dalemallen.github.io/static/css/2.0c07a3d6.chunk.css"
+  },
+  {
+    "revision": "3760ffd1299c89e4160d",
+    "url": "/dalemallen.github.io/static/css/main.4f84bccd.chunk.css"
+  },
+  {
+    "revision": "b147ff2d7b8452c32b23",
+    "url": "/dalemallen.github.io/static/js/2.47aebbf9.chunk.js"
+  },
+  {
+    "revision": "3760ffd1299c89e4160d",
+    "url": "/dalemallen.github.io/static/js/main.5a81d3e3.chunk.js"
+  },
+  {
+    "revision": "bacb804f52e757585cd9",
+    "url": "/dalemallen.github.io/static/js/runtime-main.63252cca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dalemallen.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/dalemallen.github.io/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/dalemallen.github.io/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/dalemallen.github.io/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/dalemallen.github.io/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/dalemallen.github.io/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
