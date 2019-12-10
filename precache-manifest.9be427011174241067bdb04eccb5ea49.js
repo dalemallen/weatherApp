1ea6c6fae4e74b4f557cd8fcb3a11121fbe32254
Dale The Wizard <dale.m.allen@hotmail.com>
2019-12-10 16:21:10 -0500

    Updates

---
+++ precache-manifest.9be427011174241067bdb04eccb5ea49.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900408ed9c03385065b9e42e6dee49ce",
+    "url": "/dalemallen/dalemallen.github.io.git/index.html"
+  },
+  {
+    "revision": "a0bec84cc4f68c80d0f3",
+    "url": "/dalemallen/dalemallen.github.io.git/static/css/2.8d198a1f.chunk.css"
+  },
+  {
+    "revision": "9525ca6f15039ec4fa98",
+    "url": "/dalemallen/dalemallen.github.io.git/static/css/main.4f84bccd.chunk.css"
+  },
+  {
+    "revision": "a0bec84cc4f68c80d0f3",
+    "url": "/dalemallen/dalemallen.github.io.git/static/js/2.47aebbf9.chunk.js"
+  },
+  {
+    "revision": "9525ca6f15039ec4fa98",
+    "url": "/dalemallen/dalemallen.github.io.git/static/js/main.fce944e8.chunk.js"
+  },
+  {
+    "revision": "d5342fff584ef0e92206",
+    "url": "/dalemallen/dalemallen.github.io.git/static/js/runtime-main.3837171f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dalemallen/dalemallen.github.io.git/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/dalemallen/dalemallen.github.io.git/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/dalemallen/dalemallen.github.io.git/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/dalemallen/dalemallen.github.io.git/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/dalemallen/dalemallen.github.io.git/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/dalemallen/dalemallen.github.io.git/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
